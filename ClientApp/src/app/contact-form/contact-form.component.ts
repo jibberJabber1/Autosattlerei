636ac9781ed35f540ce66783dac86e7b428aa9bc
Karolis <k.ramanauskas@hotmail.com>
2020-10-04 11:16:04 +0300

    First version of contact form design

--- ClientApp/src/app/contact-form/contact-form.component.ts
+++ ClientApp/src/app/contact-form/contact-form.component.ts
@@ -8,7 +8,8 @@ import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-contact-form',
-  templateUrl: './contact-form.component.html'
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
 })
 
 export class ContactFormComponent implements OnInit {
